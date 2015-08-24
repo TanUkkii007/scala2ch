b485e045041bdff60d72c467579bfcc93cadb07e
Yusuke Yasuda <yyusuke@trifort.jp>
2015-08-24 16:53:32 +0900

    add more primitive test

--- src/test/scala/scala2ch/models/stream/ThreadFlowTest.scala
+++ src/test/scala/scala2ch/models/stream/ThreadFlowTest.scala
@@ -11,6 +11,7 @@ import scala.concurrent.Await
 import scala2ch.TestConnectionPool
 import scala.concurrent.duration._
 import scala2ch.models.{Thread, ThreadTag, Tag}
+import akka.stream.scaladsl.{Source, Flow, Sink}
 
 class ThreadFlowTest  extends TestKit(ActorSystem("ThreadFlowITest"))
 with WordSpecLike with MustMatchers with AutoRollback with TestConnectionPool {
@@ -23,12 +24,19 @@ with WordSpecLike with MustMatchers with AutoRollback with TestConnectionPool {
 
     implicit val materializer = ActorMaterializer()
 
+    "create thread with createFlow and select with selectFlow" in { implicit session =>
+      val result = Await.result(Source.single(ThreadFlow.Create(100, "title", now)).via(ThreadStream.createFlow).runWith(Sink.head), 5 seconds)
+      (result.ownerId, result.title, result.createdAt) must be(100, "title", now)
+      val selectResult = Await.result(ThreadStream.select(result.id), 5 seconds)
+      selectResult.get.thread must be(Thread(result.id, 100, "title", now))
+    }
+
     "create thread" in { implicit session =>
       val stream = ThreadStream.createWithTag(ThreadFlow.Create(1, "title", now), List("tag1", "tag2", "tag3").map(TagFlow.Create(_, now)))
       val result = Await.result(stream, 5 seconds)
       result.thread must be(Thread(result.thread.id, 1, "title", now))
       result.tags.map(_.name) must be(Seq("tag1", "tag2", "tag3"))
-      //result.threadTags must be(Seq(ThreadTag(1,1,now), ThreadTag(1,2,now), ThreadTag(1,3,now)))
+      result.threadTags must be(Seq(ThreadTag(result.thread.id,result.tags(0).id,now), ThreadTag(result.thread.id,result.tags(1).id,now), ThreadTag(result.thread.id,result.tags(2).id,now)))
     }
 
     "select thread with tag" in { implicit session =>
